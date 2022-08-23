de2e9bcacc62f98f42f4d996995862b57ba5c39e
OSWOfficial <95764721+OSWOfficial@users.noreply.github.com>
2022-08-23 13:36:49 +0300

    Update NONDOS2.py

--- NONDOS2.py
+++ NONDOS2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup
 import os
 import time
@@ -76,82 +75,6 @@ if games == "iRacing":
         print("Download Link ---> magnet:?xt=urn:btih:503E5AA8CA90A97A0BBF48A3D7F55CDFF04F0C5C&dn=iRacing&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce")
         print("File Password ---> www.gametrex.com")
         time.sleep(9999)
-=======
-from setuptools import setup
-import os
-import time
-import turtle
 
-banner = """
-███╗   ██╗ ██████╗ ███╗   ██╗    ██████╗  ██████╗ ███████╗    ██████╗ 
-████╗  ██║██╔═══██╗████╗  ██║    ██╔══██╗██╔═══██╗██╔════╝    ╚════██╗
-██╔██╗ ██║██║   ██║██╔██╗ ██║    ██║  ██║██║   ██║███████╗     █████╔╝
-██║╚██╗██║██║   ██║██║╚██╗██║    ██║  ██║██║   ██║╚════██║    ██╔═══╝ 
-██║ ╚████║╚██████╔╝██║ ╚████║    ██████╔╝╚██████╔╝███████║    ███████╗
-╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═══╝    ╚═════╝  ╚═════╝ ╚══════╝    ╚══════╝ for Windows
-"""
-print(banner)
 
-print("""
-nonassistant - Assistant On NONDOS2 
-nongamedownloader - Free Games For PC
-""")
 
-whatyouwant = input("What Do You Want ---> ")
-
-if whatyouwant == "nonassistant":
-    nonassistant2 = input("NONDOS2 Assistant ---> ")
-    if nonassistant2 == "Hello":
-        os.system("echo off")
-        os.system("echo Hello To You Too")
-if whatyouwant == "nongamedownloader":
-    print("""
-Games :
-DIRT 4
-DIRT 5
-DIRT Rally 2.0
-DIRT Rally
-DIRT 2
-Forza Horizon 4
-Forza Horizon 5
-Nascar Heat 5
-Nascar Heat 4
-Nascar Heat 3
-iRacing
-""")
-games = input("---> ")
-if games == "DIRT 4":
-        print("Download Link ---> https://www.dosyadrive.vip/filess/1fcd9958a9e5f982bd635a74e77913f8.torrent")
-        time.sleep(9999)
-if games == "Dirt 5":
-        print("Download Link ---> https://www.dosyadrive.vip/filess/bf6327a5cf39ceb84874caa64d5d3476.torrent")
-        time.sleep(9999)
-if games == "Dirt Rally 2.0":
-        print("Download Link ---> https://www.dosyadrive.vip/filess/ab8c64ebdc0504a3fbe74ef47c5bd1c4.torrent")
-        print("File Password ---> oyunindir.vip")
-        time.sleep(9999)    
-if games == "Dirt Rally":
-        print("Download Link ---> https://www.dosyadrive.vip/filess/f053578239ec8bc443209e204ab96e79.torrent")
-        time.sleep(9999)
-if games == "Dirt 2":
-        print("Download Link ---> https://www.dosyadrive.vip/filess/81a577970586954ce712097f337f2416.torrent")
-        time.sleep(9999)
-if games == "Forza Horizon 4":
-        print("Download Link ---> https://www.dosyadrive.vip/filess/4157b6abf59acea647df649e1a7ee537.torrent")
-if games == "Forza Horizon 5":
-        print("Download Link ---> https://dosyadrive.vip/filess/e4173262f87d1b588cf5cb220f2ed752.torrent")
-        time.sleep(9999)
-if games == "Nascar Heat 5":
-        print("Download Link ---> https://dosyadrive.vip/filess/127c4a15c38a02acef8496f9499f7769.torrent")
-if games == "Nascar Heat 4":
-        print("Download Link ---> https://dosyadrive.vip/filess/47c13f18ded4a059b7d5933ee2673912.torrent")
-        print("File Password ---> oyunindir.vip")
-        time.sleep(9999)
-if games == "Nascar Heat 3":
-        print("Download Link ---> https://dosyadrive.vip/filess/7611d4f883fed3f52160bdc6622a46fa.torrent")
-        time.sleep(9999)
-if games == "iRacing":
-        print("Download Link ---> magnet:?xt=urn:btih:503E5AA8CA90A97A0BBF48A3D7F55CDFF04F0C5C&dn=iRacing&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce")
-        print("File Password ---> www.gametrex.com")
-        time.sleep(9999)
->>>>>>> adc9259173b336ed618a5816965fdf569bf41e6d
